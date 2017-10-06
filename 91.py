fc3b983ba3ff0be23b38ee7fae3acb9074a2b48a
Tong Liu <tongliu0314@gmail.com>
2017-10-06 00:10:54 -0400

    web crawler

--- 91.py
+++ 91.py
@@ -1,111 +1,88 @@
-import requests
+import requests, re, time, sys, xlwt
 from bs4 import BeautifulSoup
-import re
-import time
-import sys
-import urllib.request
-import xlwt
 from lxml import etree
-from multiprocessing import Pool
+import urllib.request
 
-def getHTMLText(url,cookies):
+def getHTMLText(url, cookies):
+
     try:
-        r = requests.get(url,cookies)
+        r = requests.get(url, cookies)
         r.raise_for_status()
         r.encoding = r.apparent_encoding
         return r.text
     except:
         print("Failed!")
 
+def getVideosInOnePage(html, worksheet, page):
 
+    soup = BeautifulSoup(html, "html.parser")
+    videoContentList = soup.find('div', attrs={'id':'videobox'})
 
+    i = 0
+    selector = etree.HTML(html)
     
-def getVideoInfo(html):
-    soup=BeautifulSoup(html,"html.parser")
-    videoContentList=soup.find('div',attrs={'id':'videobox'})
-    #print(videoContentList)#可以打印出来
+    for videoLi in videoContentList.find_all('div', attrs={'class':'listchannel'}):
 
-    videoInfoList=[]
-    
-    i=0
-    selector=etree.HTML(html)
-    
-    for videoLi in videoContentList.find_all('div',attrs={'class':'listchannel'}):
-        
-        videoName=videoLi.find('img',attrs={'width':'120'}).get('title')
-        videoUrl=videoLi.find('a',attrs={'target':'blank'}).get('href')
+        oneVideo = []
 
-        timetext=selector.xpath('//div[@class="listchannel"]/text()')[4+i*17].strip()
-        addtimetext=selector.xpath('//div[@class="listchannel"]/text()')[6+i*17].strip()
+        videoName = videoLi.find('img', attrs={'width':'120'}).get('title')
+        videoUrl = videoLi.find('a', attrs={'target':'blank'}).get('href')
+
+        timeLength = selector.xpath('//div[@class="listchannel"]/text()')[4+i*17].strip()
+        uploadTimestamp = selector.xpath('//div[@class="listchannel"]/text()')[6+i*17].strip()
         try:
-            videoAuthorContent=videoLi.find('a',attrs={'target':'_parent'}).getText()
+            authorId = videoLi.find('a', attrs={'target':'_parent'}).getText()
         except AttributeError:
-            videoAuthorContent="None"
-        
-            
-        #print(videoUrl+str(i))
+            authorId = "None"
+                    
         try:
-            videoAuthorUrl=videoLi.find('a',attrs={'target':'_parent'}).get('href')
+            authorUrl = videoLi.find('a', attrs={'target':'_parent'}).get('href')
         except AttributeError:
-            videoAuthorUrl="None"
-        viewNumber=selector.xpath('//div[@class="listchannel"]/text()')[10+i*17].strip()
-        likeNumber=selector.xpath('//div[@class="listchannel"]/text()')[11+i*17].strip()
-        commentNumber=selector.xpath('//div[@class="listchannel"]/text()')[13+i*17].strip()
+            authorUrl = "None"
+        viewCount = selector.xpath('//div[@class="listchannel"]/text()')[10+i*17].strip()
+        likeCount = selector.xpath('//div[@class="listchannel"]/text()')[11+i*17].strip()
+        commentCount = selector.xpath('//div[@class="listchannel"]/text()')[13+i*17].strip()
+
+        oneVideo.append(videoUrl)
+        oneVideo.append(videoName)
+        oneVideo.append(timeLength)
+        oneVideo.append(uploadTimestamp)
+        oneVideo.append(authorId)
+        oneVideo.append(authorUrl)
+        oneVideo.append(long(viewCount))
+        oneVideo.append(long(likeCount))
+        oneVideo.append(long(commentCount))
+
+        rownumber = (page-1) * 20 + i + 1
+        saveListToExcel(worksheet, oneVideo, rownumber)
         
-        videoInfoList.append(videoUrl)#链接
-        videoInfoList.append(videoName)#视频名
-        videoInfoList.append(timetext)#视频时长
-        videoInfoList.append(addtimetext)#上传时间
-        videoInfoList.append(videoAuthorContent)#上传者id
-        videoInfoList.append(videoAuthorUrl)#上传者主页
-        videoInfoList.append(viewNumber)#观看数
-        videoInfoList.append(likeNumber)#收藏数
-        videoInfoList.append(commentNumber)#评论数
-        
-        i+=1
-        #print(videoUrl)
-    return videoInfoList
+        i += 1
 
-
-
-def saveToExcel(videoInfoList):
-    workbook=xlwt.Workbook()
-    sheet1=workbook.add_sheet('sheet1',cell_overwrite_ok=True)
-
-    k=0
-    for i in range(10000):
-        for j in range(9):
-            print('正在写入的行和列是',i,j)
-            sheet1.write(i,j,videoInfoList[k])
-            k+=1
-    workbook.save('E:\\MyFile\\PythonSpider\\91Best\\top78000.xls')
+def saveListToExcel(worksheet, oneVideo, rownumber):
+    
+    for i, e in enumerate(oneVideo):
+        worksheet.write(rownumber, i, e)
 
 def main():
-    cookies=''#使用自己的cookies
-    top10000List=[]
-    
-    for page in range(1,505):#1到500，加5防止数组溢出
-        FvUrl='http://93.91p12.space/v.php?category=mf&viewtype=basic&page='+str(page)
-        print('正在保存的页面为第'+str(page)+'页')
-        top10000List+=getVideoInfo(getHTMLText(FvUrl,cookies))
-    saveToExcel(top10000List)
-    
+
+    workbook = xlwt.Workbook()
+    worksheet = workbook.add_sheet('worksheet', cell_overwrite_ok=True)
+
+    headers = ['videoUrl', 'videoName', 'timeLength', 'uploadTimestamp', 'authorId', 'authorUrl', 'viewCount', 'likeCount', 'commentCount']
+    for i, e in enumerate(headers):
+        worksheet.write(0, i, e)
+
+    cookies = ''
+
+    starting = 1
+    ending = 501
+
+    for page in range(starting, ending):
+        FvUrl = 'http://93.91p12.space/v.php?category=mf&viewtype=basic&page=' + str(page)
+        print('Saving video info on page ' + str(page))
+        getVideosInOnePage(getHTMLText(FvUrl, cookies), worksheet, page)
+
+    workbook.save('91_page' + str(starting) + '-' + str(ending) + '.xls')
     
 if __name__=='__main__':
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
